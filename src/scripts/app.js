f4390c6eeb7bfe9ccea88d956ac8483f631c5195
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-06-07 04:45:02 +0200

    add somes changes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,7 @@
+'use strict'
+
 const button = document.querySelector('.button__clickArea')
 button.addEventListener('click', () => {
   document.body.classList.toggle('menuOpen')
   document.activeElement.blur()
 })
-// const home = document.querySelector('.section--content:first-child')
-// const overlay = document.querySelector('.section--overlay')
