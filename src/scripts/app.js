ea3fbb00a7964fced22e7cbea4e1db6352dc275c
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-06-07 02:20:30 +0200

    add tfa case study

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,26 +1,7 @@
 const button = document.querySelector('.button__clickArea')
 button.addEventListener('click', () => {
   document.body.classList.toggle('menuOpen')
+  document.activeElement.blur()
 })
-console.log(button)
 // const home = document.querySelector('.section--content:first-child')
 // const overlay = document.querySelector('.section--overlay')
-// const h1 = document.querySelectorAll('.title__el')
-
-// window.addEventListener('load', e => {
-//   body.classList.add('container--load')
-// })
-
-// // Firefox Fix //
-// let isMozilla = navigator.userAgent.match(/mozilla|Mozilla|MOZILLA/i)
-// let isFirefox = navigator.userAgent.match(/firefox|Firefox|FIREFOX/i)
-// if (isMozilla && isFirefox) {
-//   for (let i = 0; i < h1.length; i++) {
-//     h1[i].classList.add('title__el--firefox')
-//   }
-// }
-// // Instagram In-App Browser Fix //
-// let isInstagram = navigator.userAgent.match(/instagram|Instagram|INSTAGRAM/i)
-// if (isInstagram) {
-//   body.classList.add('container--instagram')
-// }
