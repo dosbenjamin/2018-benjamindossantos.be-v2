90e3f33ef9aa8f6d85d9b89c74817d23ebd6b3f2
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-08-16 13:20:34 +0100

    add menu animations

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,57 @@
 'use strict'
 
 const button = document.querySelector('.button__clickArea')
-button.addEventListener('click', () => {
-  document.body.classList.toggle('menuOpen')
+const overlay = document.querySelector('.section--overlay')
+const links = document.querySelectorAll('.workMenu__item .link')
+let menuOpen = false
+
+button.addEventListener('click', e => {
+  const button = e.target.parentElement
+  button.disabled = true
   document.activeElement.blur()
+  document.body.classList.toggle('menuOpen')
+
+  if (menuOpen) {
+    links.forEach(link => {
+      link.classList.add('link--loadingReverse')
+      setTimeout(() => {
+        link.classList.add('link--loading')
+      }, 350)
+      setTimeout(() => {
+        link.classList.remove('link--show')
+      }, 650)
+      link.addEventListener('animationend', () => {
+        overlay.classList.add('section--alternate')
+        overlay.classList.remove('section--overlayOpen')
+      })
+    })
+    overlay.addEventListener('transitionend', () => {
+      overlay.classList.remove('section--alternate')
+      button.disabled = false
+      menuOpen = false
+      console.log(menuOpen)
+
+      console.log('close')
+    })
+  } else {
+    overlay.classList.add('section--overlayOpen')
+    links.forEach(link => {
+      link.classList.add('link--loading')
+      setTimeout(() => {
+        link.classList.add('link--loadingReverse')
+      }, 350)
+      setTimeout(() => {
+        link.classList.add('link--show')
+      }, 650)
+      link.addEventListener('animationend', e => {
+        link.classList.remove('link--loadingReverse')
+        link.classList.remove('link--loading')
+        button.disabled = false
+        menuOpen = true
+        console.log(menuOpen)
+
+        console.log('open')
+      })
+    })
+  }
 })
